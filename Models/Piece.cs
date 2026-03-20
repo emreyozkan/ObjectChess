8dbe544b96b208ddc9361d32616e8891d2738364
emreyozkan <e.yozkan0@gmail.com>
2026-03-20 14:51:52 +0100

    Added Piece class

---
+++ Models/Piece.cs
@@ -0,0 +1,28 @@
+namespace ObjectChess.Models;
+
+public enum PieceColor
+{
+    White,
+    Black
+}
+
+public abstract class Piece
+{
+    public PieceColor Color { get; private set; }
+
+    public int Row { get; protected set; }
+    public int Column { get; protected set; }
+
+    public bool HasMoved { get; protected set; }
+
+    public Piece(PieceColor color, int row, int column)
+    {
+        Color = color;
+        Row = row;
+        Column = column;
+        HasMoved = false;
+    }
+
+    public abstract bool IsValidMove(int targetRow, int targetColumn, Piece[,] board);
+}
+
