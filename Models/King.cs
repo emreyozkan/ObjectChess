cc54051f2f51fee28c5702eba8bc8f04e2ba7a07
emreyozkan <e.yozkan0@gmail.com>
2026-04-07 00:45:45 +0200

    Add King class to represent king piece and implement movement validation

---
+++ Models/King.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ObjectChess.Models;
+
+public class King : Piece
+{
+    public King(PieceColor color, int row, int column) : base(color, row, column)
+    {
+    }
+
+    public override bool IsValidMove(int targetRow, int targetColumn, Piece[,] board)
+    {
+        int rowDiff = Math.Abs(targetRow - Row);
+        int columnDiff = Math.Abs(targetColumn - Column);
+
+        if (rowDiff <= 1 && columnDiff <= 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
