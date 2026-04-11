a8324a23e57e424096e654002c607b0cc9f09105
emreyozkan <e.yozkan0@gmail.com>
2026-04-11 16:02:06 +0200

    Add MatchController to handle match history retrieval

---
+++ Controllers/MatchController.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using ObjectChess.Models;
+
+namespace ObjectChess.Controllers
+{
+    public class MatchController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public MatchController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult History()
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            
+            List<MatchHistoryViewModel> matchHistoryList = new List<MatchHistoryViewModel>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @"
+                    SELECT 
+                        White.Username AS WhitePlayer, 
+                        Black.Username AS BlackPlayer, 
+                        Winner.Username AS Winner, 
+                        g.MatchDate 
+                    FROM Games g
+                    JOIN Players White ON g.WhitePlayerID = White.PlayerID
+                    JOIN Players Black ON g.BlackPlayerID = Black.PlayerID
+                    LEFT JOIN Players Winner ON g.WinnerID = Winner.PlayerID
+                    ORDER BY g.MatchDate DESC;";
+                
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MatchHistoryViewModel match = new MatchHistoryViewModel();
+
+                            match.WhitePlayer = reader["WhitePlayer"].ToString();
+                            match.BlackPlayer = reader["BlackPlayer"].ToString();
+                            match.MatchDate = Convert.ToDateTime(reader["MatchDate"]);
+
+                            if (reader["Winner"] != DBNull.Value)
+                            {
+                                match.Winner = reader["Winner"].ToString();
+                            }
+                            else
+                            {
+                                match.Winner = "Draw";
+                            }
+
+                            matchHistoryList.Add(match);
+                        }
+                    }
+                }
+            }
+
+            return View(matchHistoryList);
+        }
+    }
+}
